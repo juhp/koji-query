a2dec280c5ca4a4df1448b81523378a3363c8696
Jens Petersen <petersen@redhat.com>
2021-07-13 02:14:02 +0800

    refactor result with TaskResult type

--- src/Main.hs
+++ src/Main.hs
@@ -53,21 +53,36 @@ program muser limit archs mdate = do
     printTask :: TimeZone -> Struct -> IO ()
     printTask tz task = do
       putStrLn ""
-      whenJust (taskLines tz task) (mapM_ putStrLn)
+      whenJust (taskLines task) $ mapM_ putStrLn . formatTaskResult tz
+
+    formatTaskResult :: TimeZone -> TaskResult -> [String]
+    formatTaskResult tz (TaskResult title url start mcompletion) =
+      [ title
+      , url
+      , formatTime defaultTimeLocale "%c" (utcToLocalTime tz start)
+      ]
+      ++
+      case mcompletion of
+        Nothing -> []
+        Just c_t ->
+          [formatTime defaultTimeLocale "%c" (utcToLocalTime tz c_t)]
+#if MIN_VERSION_time(1,9,1)
+          ++
+          let dur = diffUTCTime c_t start
+          in ["duration: " ++ formatTime defaultTimeLocale "%Hh %Mm %Ss" dur]
+#endif
 
     readTime' :: String -> UTCTime
     readTime' = read . replace "+00:00" "Z"
 
-    taskLines :: TimeZone -> Struct -> Maybe [String]
-    taskLines tz st = do
+    taskLines :: Struct -> Maybe TaskResult
+    taskLines st = do
       arch <- lookupStruct "arch" st
       start_time <- readTime' <$> lookupStruct "start_time" st
       let mcompletion_time = readTime' <$> lookupStruct "completion_time" st
       taskid <- lookupStruct "id" st
       method <- lookupStruct "method" st
       state <- getTaskState st
-#if MIN_VERSION_time(1,9,1)
-#endif
       request <- head <$> lookupStruct "request" st >>= getString
       let package =
             if method == "buildArch"
@@ -75,16 +90,10 @@ program muser limit archs mdate = do
             else takeFileName request
           parent = lookupStruct "parent" st :: Maybe Int
       return $
-        [package +-+ method +-+ show state +-+ maybe "" show parent,
-         "https://koji.fedoraproject.org/koji/taskinfo?taskID=" ++ show (taskid :: Int),
-         formatTime defaultTimeLocale "%c" (utcToLocalTime tz start_time)]
-         ++
-         case mcompletion_time of
-           Nothing -> []
-           Just c_t ->
-             [formatTime defaultTimeLocale "%c" (utcToLocalTime tz c_t)]
-#if MIN_VERSION_time(1,9,1)
-             ++
-             let dur = diffUTCTime c_t start_time
-             in ["duration: " ++ formatTime defaultTimeLocale "%Hh %Mm %Ss" dur]
-#endif
+        TaskResult
+          (package +-+ method +-+ show state +-+ maybe "" show parent)
+          ("https://koji.fedoraproject.org/koji/taskinfo?taskID=" ++ show (taskid :: Int))
+          start_time
+          mcompletion_time
+
+data TaskResult = TaskResult String String UTCTime (Maybe UTCTime)
