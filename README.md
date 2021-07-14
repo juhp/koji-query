f4264f2b8abbb15a0f424e49b9985244363634d4
Jens Petersen <petersen@redhat.com>
2021-07-14 23:59:53 +0800

    refactoring and more options: server, taskid, date, method and pkg filter
    - setupQuery
    - use rpm-nvr
    - hlint

--- README.md
+++ README.md
@@ -10,4 +10,39 @@ koji-buildlog-sizes (possibly even koji-install).
 
 ## Installation
 
-`cabal-rpm install`, `cabal install` or `stack install`.
+Either: `cabal install  --installdir=~/bin`
+
+(on Fedora first run `cabal-rpm builddep` to save a lot of building).
+
+or: `stack install`.
+
+## Usage
+
+By default it shows your Fedora Koji tasks since yesterday.
+
+```
+$ koji-query --version
+0.1.0
+$ koji-query --help
+koji-query
+
+Usage: koji-query [--version] [-S|--server URL] [-u|--user USER]
+                  [-l|--limit INT] [(-t|--task TASKID) | (-P|--parent TASKID)]
+                  [-s|--state STATE] [-a|--arch ARCH] [-d|--date DAY]
+                  [-m|--method METHOD] [-p|--package PKG]
+  Helper client for koji queries: https://github.com/juhp/koji-query
+
+Available options:
+  -h,--help                Show this help text
+  --version                Show version
+  -S,--server URL          Koji Hub [default: Fedora]
+  -u,--user USER           Koji user
+  -l,--limit INT           Maximum number of tasks to show [default: 20]
+  -t,--task TASKID         Show task
+  -P,--parent TASKID       List children tasks
+  -s,--state STATE         Filter tasks by state
+  -a,--arch ARCH           Task arch
+  -d,--date DAY            Tasks started after date [default: yesterday]
+  -m,--method METHOD       Select tasks by method: [build,buildarch,etc]
+  -p,--package PKG         Filter results to specified package
+```
--- src/Main.hs
+++ src/Main.hs
@@ -11,6 +11,7 @@ import Data.Maybe
 #if !MIN_VERSION_base(4,11,0)
 import Data.Monoid ((<>))
 #endif
+import Data.RPM.NVR
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Text.Format.Numbers
@@ -26,57 +27,74 @@ import System.FilePath
 
 import Paths_koji_query (version)
 
--- FIXME query a taskid!
+data TaskIdReq = Task Int | Parent Int | TaskQuery
+
 main :: IO ()
 main =
-  simpleCmdArgs' (Just version) "koji-query" "Helper client for koji queries" $
+  simpleCmdArgs' (Just version) "koji-query"
+  "Helper client for koji queries: https://github.com/juhp/koji-query" $
   program
-  <$> optional (strOptionWith 'u' "user" "USER" "Koji user")
-  <*> optionalWith auto 'l' "limit" "NUMTASKS" "Maximum number of tasks to show [default: 20]" 20
-  <*> optional (optionWith auto 'p' "parent" "TASKID" "List children tasks")
+  <$> strOptionalWith 'S' "server" "URL" "Koji Hub [default: Fedora]" fedoraKojiHub
+  <*> optional (strOptionWith 'u' "user" "USER" "Koji user")
+  <*> optionalWith auto 'l' "limit" "INT" "Maximum number of tasks to show [default: 20]" 20
+  <*> (Task <$> optionWith auto 't' "task" "TASKID" "Show task"
+       <|> Parent <$> optionWith auto 'P' "parent" "TASKID" "List children tasks"
+       <|> pure TaskQuery)
   <*> many (parseTaskState <$> strOptionWith 's' "state" "STATE" "Filter tasks by state")
   <*> many (strOptionWith 'a' "arch" "ARCH" "Task arch")
-  <*> optional (strArg "DAY")
-
-program :: Maybe String -> Int -> Maybe Int -> [TaskState] -> [String]
-        -> Maybe String
+  <*> optional (strOptionWith 'd' "date" "DAY" "Tasks started after date [default: yesterday]")
+  <*> optional (strOptionWith 'm' "method" "METHOD" "Select tasks by method: [build,buildarch,etc]")
+  <*> optional (strOptionWith 'p' "package" "PKG" "Filter results to specified package")
+program :: String -> Maybe String -> Int -> TaskIdReq -> [TaskState]
+        -> [String] -> Maybe String -> Maybe String -> Maybe String
         -> IO ()
-program muser limit mparent states archs mdate = do
+program server muser limit taskreq states archs mdate mmethod mpkg = do
   tz <- getCurrentTimeZone
   mgr <- httpManager
-  case mparent of
-    Just parent -> do
-      when (isJust muser || isJust mdate) $
-        error' $ "cannot use --parent together with --user or date"
-      listTasks fedoraKojiHub
-        ([("parent", ValueInt parent),
-          ("decode", ValueBool True)]
-         ++ [("state", ValueArray (map taskStateToValue states)) | notNull states]
-         ++ [("arch", ValueArray (map ValueString archs)) | notNull archs])
-        [("order", ValueString "id")]
-        >>= mapM_ (printTask mgr tz)
-    Nothing -> do
-      date <- cmd "date" ["+%F", "--date=" ++ dateString mdate]
-      user <- case muser of
-                Just user -> return user
-                Nothing -> do
-                  mfasid <- (dropSuffix "@FEDORAPROJECT.ORG" <$>) . find ("@FEDORAPROJECT.ORG" `isSuffixOf`) . words <$> cmd "klist" ["-l"]
-                  case mfasid of
-                    Just fas -> return fas
-                    Nothing -> error' "Could not determine FAS id from klist"
-      mowner <- kojiGetUserID fedoraKojiHub user
-      case mowner of
-        Nothing -> error "No owner found"
-        Just owner -> do
-          listTasks fedoraKojiHub
-            ([("owner", ValueInt (getID owner)),
-              ("startedAfter", ValueString date),
-              ("decode", ValueBool True)]
-             ++ [("state", ValueArray (map taskStateToValue states)) | notNull states]
-             ++ [("arch", ValueArray (map ValueString archs)) | notNull archs])
-            [("limit",ValueInt limit), ("order", ValueString "id")]
-            >>= mapM_ (printTask mgr tz)
+  case taskreq of
+    Task taskid -> do
+      mtask <- kojiGetTaskInfo server (TaskId taskid)
+      whenJust mtask
+        $ \task -> whenJust (maybeTaskResult task)
+                   $ printTask mgr tz
+    _ -> do
+      query <- setupQuery
+      results <- listTasks server query
+                 [("limit",ValueInt limit), ("order", ValueString "id")]
+      (mapM_ (printTask mgr tz) . filterResults . mapMaybe maybeTaskResult) results
   where
+    setupQuery = do
+      case taskreq of
+        Task _ -> error' "reachable task request"
+        Parent parent -> do
+          when (isJust muser || isJust mdate || isJust mpkg) $
+            error' "cannot use --parent together with --user, --date, or --package"
+          return $
+            ("parent", ValueInt parent) : commonParams
+        TaskQuery -> do
+          date <- cmd "date" ["+%F", "--date=" ++ dateString mdate]
+          user <- case muser of
+                    Just user -> return user
+                    Nothing -> do
+                      mfasid <- (dropSuffix "@FEDORAPROJECT.ORG" <$>) . find ("@FEDORAPROJECT.ORG" `isSuffixOf`) . words <$> cmd "klist" ["-l"]
+                      case mfasid of
+                        Just fas -> return fas
+                        Nothing -> error' "Could not determine FAS id from klist"
+          mowner <- kojiGetUserID fedoraKojiHub user
+          case mowner of
+            Nothing -> error "No owner found"
+            Just owner ->
+              return $
+                [("owner", ValueInt (getID owner)),
+                 ("startedAfter", ValueString date)]
+                ++ commonParams
+        where
+          commonParams =
+            [("decode", ValueBool True)]
+            ++ [("state", ValueArray (map taskStateToValue states)) | notNull states]
+            ++ [("arch", ValueArray (map ValueString archs)) | notNull archs]
+            ++ [("method",  ValueString method) | Just method <- [mmethod]]
+
     dateString :: Maybe String -> String
     dateString Nothing = "yesterday"
     dateString (Just s) =
@@ -84,62 +102,69 @@ program muser limit mparent states archs mdate = do
       then "last " ++ s
       else s
 
-    printTask :: Manager -> TimeZone -> Struct -> IO ()
-    printTask mgr tz task = do
-      putStrLn ""
-      whenJust (taskResult task) $ \ result -> do
-        let mendtime = mtaskEndTime result
-        time <- case mendtime of
-          Just end -> return end
-          Nothing -> getCurrentTime
-        (mapM_ putStrLn . formatTaskResult (isJust mendtime) tz) (result {mtaskEndTime = Just time})
-        buildlogSize mgr (taskId result)
-
-    formatTaskResult :: Bool -> TimeZone -> TaskResult -> [String]
-    formatTaskResult ended tz (TaskResult package method state mparent' taskid start mendtime) =
-      [ package +-+ method +-+ show state +-+ maybe "" (\p -> "(parent: " ++ show p ++ ")") mparent'
-      , "https://koji.fedoraproject.org/koji/taskinfo?taskID=" ++ show taskid
-      , formatTime defaultTimeLocale "%c (start)" (utcToLocalTime tz start)
-      ]
-      ++
-      case mendtime of
-        Nothing -> []
-        Just end ->
-          [formatTime defaultTimeLocale "%c (end)" (utcToLocalTime tz end) | ended]
-#if MIN_VERSION_time(1,9,1)
-          ++
-          let dur = diffUTCTime end start
-          in [(if not ended then "current " else "") ++ "duration: " ++ formatTime defaultTimeLocale "%Hh %Mm %Ss" dur]
-#endif
-
-    readTime' :: String -> UTCTime
-    readTime' = read . replace "+00:00" "Z"
-
-    taskResult :: Struct -> Maybe TaskResult
-    taskResult st = do
+    maybeTaskResult :: Struct -> Maybe TaskResult
+    maybeTaskResult st = do
       arch <- lookupStruct "arch" st
       start_time <- readTime' <$> lookupStruct "start_time" st
       let mend_time = readTime' <$> lookupStruct "completion_time" st
       taskid <- lookupStruct "id" st
       method <- lookupStruct "method" st
       state <- getTaskState st
-      request <- head <$> lookupStruct "request" st >>= getString
-      let package =
+      request <- lookupStruct "request" st >>= getString . head
+      let nvr =
             if method == "buildArch"
-            then removeSuffix ".src.rpm" (takeFileName request) <.> arch
+            then removeSuffix ".src.rpm" (takeFileName request)
             else takeFileName request
           mparent' = lookupStruct "parent" st :: Maybe Int
       return $
-        TaskResult package method state mparent' taskid start_time mend_time
+        TaskResult (readNVR nvr) arch method state mparent' taskid start_time mend_time
+      where
+        readTime' :: String -> UTCTime
+        readTime' = read . replace "+00:00" "Z"
+
+    filterResults :: [TaskResult] -> [TaskResult]
+    filterResults ts =
+      case mpkg of
+        Nothing -> ts
+        Just pkg -> filter (isPackage pkg . taskNVR) ts
+      where
+        isPackage pkg (NVR n _) = n == pkg
+
+    printTask :: Manager -> TimeZone -> TaskResult -> IO ()
+    printTask mgr tz task = do
+      putStrLn ""
+      let mendtime = mtaskEndTime task
+      time <- maybe getCurrentTime return mendtime
+      (mapM_ putStrLn . formatTaskResult (isJust mendtime) tz) (task {mtaskEndTime = Just time})
+      buildlogSize mgr (taskId task)
+
+formatTaskResult :: Bool -> TimeZone -> TaskResult -> [String]
+formatTaskResult ended tz (TaskResult nvr arch method state mparent taskid start mendtime) =
+  [ showNVR nvr +-+ arch +-+ method +-+ show state +-+ maybe "" (\p -> "(parent: " ++ show p ++ ")") mparent
+  , "https://koji.fedoraproject.org/koji/taskinfo?taskID=" ++ show taskid
+  , formatTime defaultTimeLocale "%c (start)" (utcToLocalTime tz start)
+  ]
+  ++
+  case mendtime of
+    Nothing -> []
+    Just end ->
+      [formatTime defaultTimeLocale "%c (end)" (utcToLocalTime tz end) | ended]
+#if MIN_VERSION_time(1,9,1)
+      ++
+      let dur = diffUTCTime end start
+      in [(if not ended then "current " else "") ++ "duration: " ++ formatTime defaultTimeLocale "%Hh %Mm %Ss" dur]
+#endif
+
 
 data TaskResult =
-  TaskResult {_taskPackage :: String,
+  TaskResult {taskNVR :: NVR,
+              _taskArch :: String,
               _taskMethod :: String,
               _taskState :: TaskState,
               _mtaskParent :: Maybe Int,
               taskId :: Int,
               _taskStartTime :: UTCTime,
-              mtaskEndTime :: (Maybe UTCTime)
+              mtaskEndTime :: Maybe UTCTime
              }
 
 ----
@@ -173,7 +198,7 @@ buildlogSize mgr taskid = do
       putStr "("
       (T.putStr . kiloBytes) size
       putStrLn ")"
-      -- FIXME if too small show root.log instead
+      -- FIXME if too small show root.log url instead
       putStrLn logtail
   where
     tid = show taskid
